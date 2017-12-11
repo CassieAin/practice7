ed4aa02dc42b510199412316a68a23021ab6196e
CassieAin <roksolana.d22@gmail.com>
2017-12-11 18:41:55 +0200

    RedBlackTree implementation

--- src/main/java/task7_3/Main3.java
+++ src/main/java/task7_3/Main3.java
@@ -1,10 +1,9 @@
 package task7_3;
 
-import task7_2.UserInterface;
-
 public class Main3 {
 
-    public static void main(String []args){
-
+    public static void main(String []args) {
+        RedBlackTree rbtree = new RedBlackTree();
+        rbtree.run();
     }
 }
